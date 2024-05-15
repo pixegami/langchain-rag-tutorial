dba6c139701f2eefba563a5a667a5336f112575a
Deepam Makwana <dpammkwana@gmail.com>
2024-05-15 10:55:04 +0530

    Update create_database.py
    Changed deprecated imports

--- create_database.py
+++ create_database.py
@@ -1,13 +1,14 @@
-from langchain.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores.chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
 import os
 import shutil
 
 CHROMA_PATH = "chroma"
 DATA_PATH = "data/books"
+os.environ["OPENAI_API_KEY"] = "YOUR-API-KEY"
 
 
 def main():
